dbeb3d7b27b22b29808421ca5dd5ae910fba8203
Ilya Ternovykh <ily1925@yandex.ru>
2021-08-26 19:45:37 +0300

    feat: добавить подключение к бд

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { CURRENT_PORT } = require('./utils/config');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(CURRENT_PORT, () => {
   // eslint-disable-next-line no-console
