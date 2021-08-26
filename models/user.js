137f84184f308a2e35a6586bd4ba1994942a7275
Ilya Ternovykh <ily1925@yandex.ru>
2021-08-26 22:05:23 +0300

    feat: создать схемы и модели ресурсов

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const BadRequestError = require('../errors/bad-request-err');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new BadRequestError('Некорректный Email');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  __v: {
+    type: Number,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUser(email, password) {
+  // поиск пользователя по почте
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Передан неверный логин или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Передан неверный логин или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+userSchema.pre('save', function savePass(next) {
+  if (!this.isModified('password')) return next();
+
+  return bcrypt.hash(this.password, 10)
+    .then((hash) => {
+      this.password = hash;
+
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = mongoose.model('user', userSchema);
